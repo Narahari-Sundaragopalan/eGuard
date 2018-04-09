8b44511e3e3322ea8614733f180e9abaa8ae7782
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2018-04-09 15:08:43 -0500

    Setup for connecting BLE MW and Accelerometer data

--- code/app/src/main/java/eguard/android/hari/com/myapplication/MainActivity.java
+++ code/app/src/main/java/eguard/android/hari/com/myapplication/MainActivity.java
@@ -1,13 +1,110 @@
 package eguard.android.hari.com.myapplication;
 
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.mbientlab.metawear.Data;
+import com.mbientlab.metawear.MetaWearBoard;
+import com.mbientlab.metawear.Route;
+import com.mbientlab.metawear.Subscriber;
+import com.mbientlab.metawear.android.BtleService;
+import com.mbientlab.metawear.builder.RouteBuilder;
+import com.mbientlab.metawear.builder.RouteComponent;
+import com.mbientlab.metawear.data.Acceleration;
+import com.mbientlab.metawear.module.Accelerometer;
+
+import bolts.Continuation;
+import bolts.Task;
+
+public class MainActivity extends AppCompatActivity implements ServiceConnection {
+
+    private BtleService.LocalBinder serviceBinder;
+    private MetaWearBoard board;
+    private Accelerometer accelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ///< Bind the service when the activity is created
+        getApplicationContext().bindService(new Intent(this, BtleService.class),
+                this, Context.BIND_AUTO_CREATE);
     }
-}
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        ///< Unbind the service when the activity is destroyed
+        getApplicationContext().unbindService(this);
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+
+        ///< Typecast the binder to the service's LocalBinder class
+        serviceBinder = (BtleService.LocalBinder) service;
+
+        Log.i("eGuard", "Service Connected");
+
+        // Metawear Mac address
+        retrieveBoard("D0:58:8E:AA:76:8D");
+
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+
+    }
+
+    private void retrieveBoard(String macAddr) {
+        final BluetoothManager btManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        final BluetoothDevice remoteDevice =
+                btManager.getAdapter().getRemoteDevice(macAddr);
+
+        //Create Metawear board object for the bluetooth device
+        board = serviceBinder.getMetaWearBoard(remoteDevice);
+        board.connectAsync().onSuccessTask(new Continuation<Void, Task<Route>>() {
+            @Override
+            public Task<Route> then(Task<Void> task) throws Exception {
+                Log.i("eGuard", "Connected to Metawear board successfully");
+
+                accelerometer = board.getModule(Accelerometer.class);
+                accelerometer.configure()
+                        .odr(25f)
+                        .commit();
+                return accelerometer.acceleration().addRouteAsync(new RouteBuilder() {
+                    @Override
+                    public void configure(RouteComponent source) {
+                        source.stream(new Subscriber() {
+                            @Override
+                            public void apply(Data data, Object... env) {
+                                Log.i("eGuard", data.value(Acceleration.class).toString());
+                            }
+                        });
+                    }
+                });
+            }
+        }).continueWith(new Continuation<Route, Void>() {
+            @Override
+            public Void then(Task<Route> task)  throws Exception {
+                if(task.isFaulted()) {
+                    Log.w("eGuard", "Failed to configure app", task.getError());
+                } else {
+                    Log.i("eGuard", "App configured");
+                }
+                return null;
+            }
+        });
+    }
+ }
