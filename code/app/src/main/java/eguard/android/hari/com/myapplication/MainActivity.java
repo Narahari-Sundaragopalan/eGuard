a27455294f802f46b0f37a091581a3fa3f99cf96
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2018-04-14 23:49:26 -0500

    Basic app working to detect fall of metawear

--- code/app/src/main/java/eguard/android/hari/com/myapplication/MainActivity.java
+++ code/app/src/main/java/eguard/android/hari/com/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package eguard.android.hari.com.myapplication;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.content.ComponentName;
@@ -10,6 +11,7 @@ import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.mbientlab.metawear.Data;
 import com.mbientlab.metawear.MetaWearBoard;
@@ -18,47 +20,68 @@ import com.mbientlab.metawear.Subscriber;
 import com.mbientlab.metawear.android.BtleService;
 import com.mbientlab.metawear.builder.RouteBuilder;
 import com.mbientlab.metawear.builder.RouteComponent;
+import com.mbientlab.metawear.builder.filter.Comparison;
+import com.mbientlab.metawear.builder.filter.ThresholdOutput;
+import com.mbientlab.metawear.builder.function.Function1;
 import com.mbientlab.metawear.data.Acceleration;
 import com.mbientlab.metawear.module.Accelerometer;
 
 import bolts.Continuation;
 import bolts.Task;
 
-public class MainActivity extends AppCompatActivity implements ServiceConnection {
-
+public class MainActivity extends Activity implements ServiceConnection {
     private BtleService.LocalBinder serviceBinder;
+    private final String MW_MAC_ADDRESS = "D0:58:8E:AA:76:8D";
     private MetaWearBoard board;
-    private Accelerometer accelerometer;
+    Accelerometer accelerometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ///< Bind the service when the activity is created
         getApplicationContext().bindService(new Intent(this, BtleService.class),
                 this, Context.BIND_AUTO_CREATE);
+
+        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("eGuard", "start");
+                accelerometer.acceleration().start();
+                accelerometer.start();
+            }
+        });
+
+        findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("eGuard", "stop");
+                accelerometer.stop();
+                accelerometer.acceleration().stop();
+            }
+        });
+        findViewById(R.id.reset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                board.tearDown();
+            }
+        });
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
 
-        ///< Unbind the service when the activity is destroyed
+        // Unbind the service when the activity is destroyed
         getApplicationContext().unbindService(this);
     }
 
     @Override
     public void onServiceConnected(ComponentName name, IBinder service) {
-
-        ///< Typecast the binder to the service's LocalBinder class
         serviceBinder = (BtleService.LocalBinder) service;
 
         Log.i("eGuard", "Service Connected");
-
-        // Metawear Mac address
         retrieveBoard("D0:58:8E:AA:76:8D");
-
     }
 
     @Override
@@ -66,40 +89,50 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
 
     }
 
-    private void retrieveBoard(String macAddr) {
-        final BluetoothManager btManager =
+    public void retrieveBoard(final String macAddr) {
+        final BluetoothManager btManager=
                 (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-        final BluetoothDevice remoteDevice =
+        final BluetoothDevice remoteDevice=
                 btManager.getAdapter().getRemoteDevice(macAddr);
 
-        //Create Metawear board object for the bluetooth device
-        board = serviceBinder.getMetaWearBoard(remoteDevice);
+        // Create a MetaWear board object for the Bluetooth Device
+        board= serviceBinder.getMetaWearBoard(remoteDevice);
         board.connectAsync().onSuccessTask(new Continuation<Void, Task<Route>>() {
             @Override
             public Task<Route> then(Task<Void> task) throws Exception {
-                Log.i("eGuard", "Connected to Metawear board successfully");
 
-                accelerometer = board.getModule(Accelerometer.class);
+                Log.i("eGuard", "Connected to " + macAddr);
+
+                accelerometer= board.getModule(Accelerometer.class);
                 accelerometer.configure()
-                        .odr(25f)
+                        .odr(60f)       // Set sampling frequency to 25Hz, or closest valid ODR
                         .commit();
                 return accelerometer.acceleration().addRouteAsync(new RouteBuilder() {
                     @Override
                     public void configure(RouteComponent source) {
-                        source.stream(new Subscriber() {
+                        source.map(Function1.RSS).average((byte) 4).filter(ThresholdOutput.BINARY, 0.5f)
+                                .multicast()
+                                .to().filter(Comparison.EQ, -1).stream(new Subscriber() {
                             @Override
                             public void apply(Data data, Object... env) {
-                                Log.i("eGuard", data.value(Acceleration.class).toString());
-                            }
-                        });
+                                Log.i("eGuard", "in freefall");
+                                }
+                            })
+                                .to().filter(Comparison.EQ, 1).stream(new Subscriber() {
+                            @Override
+                            public void apply(Data data, Object... env) {
+                                Log.i("eGuard", "Not in freefall");
+                                }
+                            })
+                        .end();
                     }
                 });
             }
         }).continueWith(new Continuation<Route, Void>() {
             @Override
-            public Void then(Task<Route> task)  throws Exception {
+            public Void then(Task<Route> task) throws Exception {
                 if(task.isFaulted()) {
-                    Log.w("eGuard", "Failed to configure app", task.getError());
+                    Log.w("eGuard", "Failed to configure app ", task.getError());
                 } else {
                     Log.i("eGuard", "App configured");
                 }
@@ -107,4 +140,4 @@ public class MainActivity extends AppCompatActivity implements ServiceConnection
             }
         });
     }
- }
+}
