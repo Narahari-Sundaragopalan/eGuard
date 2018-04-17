27ab1fbc0fc469f5701801ab0f5c763bb2de61e8
Narahari Sundaragopalan <nsundaragopalan@unomaha.edu>
2018-04-17 01:21:18 -0500

    Added some extra comments for threshold

--- code/app/src/main/java/eguard/android/hari/com/myapplication/MainActivity.java
+++ code/app/src/main/java/eguard/android/hari/com/myapplication/MainActivity.java
@@ -1,4 +1,6 @@
-// eGuard Application to detect fall and raise alerts in case of emergencies
+/*  eGuard Application to detect fall and raise alerts in case of emergencies
+    Check the repo README file for more information on the application
+*/
 
 package eguard.android.hari.com.myapplication;
 
@@ -36,7 +38,10 @@ public class MainActivity extends Activity implements ServiceConnection {
     //connect app to Bluetooth Service
     private BtleService.LocalBinder serviceBinder;
 
-    // Device MAC Address
+    /* Device MAC Address. As the bluetooth scan is weak and not reliable, passing the device MAC address
+       to ensure simple connectivity for now
+       TODO: Probably not a secure way to expose device MAC Address, change it and test for Bluetooth Scan
+    */
     private final String MW_MAC_ADDRESS = "D0:58:8E:AA:76:8D";
 
     //MetaWear object to connect app and device
@@ -53,8 +58,9 @@ public class MainActivity extends Activity implements ServiceConnection {
         getApplicationContext().bindService(new Intent(this, BtleService.class),
                 this, Context.BIND_AUTO_CREATE);
 
-        // Event Listener for Start Button
-        // When clicked, accelerometer will start monitoring the user acceleration
+        /* Event Listener for Start Button
+           When clicked, accelerometer will start monitoring the user acceleration
+        */
         findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,8 +70,8 @@ public class MainActivity extends Activity implements ServiceConnection {
             }
         });
 
-        // Event Listener for Stop Button
-        // When clicked, accelerometer will stop monitoring the user acceleration
+        /* Event Listener for Stop Button
+           When clicked, accelerometer will stop monitoring the user acceleration */
         findViewById(R.id.stop).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -107,9 +113,10 @@ public class MainActivity extends Activity implements ServiceConnection {
 
     }
 
-    //
-    // Data is monitored and processed as part of this function and threshold value is checked
-    //
+    /*
+      Data is monitored and processed as part of this function and threshold value is checked
+    */
+
     // Function will connect to the MetaWear device
     public void retrieveBoard(final String macAddr) {
         final BluetoothManager btManager=
@@ -120,7 +127,7 @@ public class MainActivity extends Activity implements ServiceConnection {
         // Create a MetaWear board object for the Bluetooth Device
         board = serviceBinder.getMetaWearBoard(remoteDevice);
 
-        // Make a connection to the MetaWear device
+        // Make a connection to the MetaWear device using its MAC address
         board.connectAsync().onSuccessTask(new Continuation<Void, Task<Route>>() {
             @Override
             public Task<Route> then(Task<Void> task) throws Exception {
@@ -133,7 +140,18 @@ public class MainActivity extends Activity implements ServiceConnection {
                         .odr(12.5f)
                         .commit();
 
-                // Check the acceleration data if threshold value has been reached
+                /*
+                    Check the acceleration data if threshold value has been reached across 4 samples
+                    The threshold value is set to be 0.4g
+                    This means when there is acceleration equal or more than 0.4g, the
+                    app will identify this as a fall scenario and raise an alert
+                    Threshold value was obtained after collecting and monitoring
+                    multiple scenarios and transitions in user movements
+                    Check the data-source folder README file in the repo for more information on this
+                    Using Binary Mode to check for Threshold output, Return value will be 1 when threshold is reached
+                    and -1 when within threshold
+                */
+
                 return accelerometer.acceleration().addRouteAsync(new RouteBuilder() {
                     @Override
                     public void configure(RouteComponent source) {
@@ -141,15 +159,17 @@ public class MainActivity extends Activity implements ServiceConnection {
                                 .multicast()
                                 .to().filter(Comparison.EQ, -1).stream(new Subscriber() {
                             @Override
-                            // If the user acceleration has gone beyond the threshold, log a fall message
-                            // And raise an alert to send a message
+                            /*
+                               If the user acceleration has gone beyond the threshold, log a fall message
+                               TODO: And raise an alert to send a message
+                            */
                             public void apply(Data data, Object... env) {
                                 Log.i("eGuard", "There has been a fall");
                                 }
                             })
                                 .to().filter(Comparison.EQ, 1).stream(new Subscriber() {
                             @Override
-                            // If the user movement is normal, log
+                            // If the user movement is normal, log a message
                             public void apply(Data data, Object... env) {
                                 Log.i("eGuard", "Normal user movement");
                                 }
